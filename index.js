dbd0bab729ddaf9d83b2003222d8d8721f17013d
Davith Chhung <chhungdavith@gmail.com>
2022-05-23 13:56:14 +0200

    add fetch all taches route

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const db = require("./src/db/db");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  console.log("server is stated");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/taches", (req, res) => {
+  res.send(200, db.getAll());
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port : ${PORT}`);
 });
